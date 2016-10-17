560f8cecf96ca1349ebf39a9b283c2c26f4717ff
alvidaIsHere <rivalvi.da@hotmail.com>
2016-10-17 20:40:05 +1000

    Patch from previous update: missing file

--- TestDemo.Droid/Views/GoalUpdateView.cs
+++ TestDemo.Droid/Views/GoalUpdateView.cs
@@ -7,6 +7,8 @@ using Android.App;
 using Android.OS;
 
 using MvvmCross.Droid.Views;
+using Android.Widget;
+using TestDemo.Core.ViewModels;
 
 namespace TestDemo.Droid.Views
 {
@@ -19,6 +21,47 @@ namespace TestDemo.Droid.Views
             SetContentView(Resource.Layout.GoalUpdateView);
 
             // Create your application here
+            GoalUpdateViewModel vm = (GoalUpdateViewModel)ViewModel;
+            Button btnDelete = FindViewById<Button>(Resource.Id.button_delete);
+            Button btnComplete = FindViewById<Button>(Resource.Id.button_complete);
+
+
+            if (vm.Status.Equals("STARTED"))
+            {
+                btnComplete.Visibility = Android.Views.ViewStates.Visible;
+                btnDelete.Visibility = Android.Views.ViewStates.Visible;
+            }else
+            {
+                //for COMPLETED DELETED EXPIRED goal
+                btnComplete.Visibility = Android.Views.ViewStates.Gone;
+                btnDelete.Visibility = Android.Views.ViewStates.Gone;
+            }
+
+            string message = "toast mesage";
+            btnComplete.Click += delegate
+            {
+
+                if (btnComplete.Text.Equals("Restore"))
+                {
+                    message = "Goal re-started";
+
+                }
+                else
+                {
+                    message = "Congrats :D goal completed!";
+
+                }
+                Toast.MakeText(this, message, ToastLength.Short).Show();
+                
+            };
+
+            btnDelete.Click += delegate
+            {
+                message = "Goal Deleted";
+                Toast.MakeText(this, message, ToastLength.Short).Show();
+
+            };
+
         }
     }
 }
